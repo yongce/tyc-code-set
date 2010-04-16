b9b2b6f56ca1be1dbc593e37f1da6825deb28fda
Yongce Tu <yongce.tu@gmail.com>
2010-04-16 17:20:22 +0000

    Ag
    git-svn-id: http://tyc-code-set.googlecode.com/svn/trunk@14 028cf64e-a2a0-11dd-a9c7-49dec45ca499

--- TuLib/UniqueNameGenerator/UniqueNameGenerator.cpp
+++ TuLib/UniqueNameGenerator/UniqueNameGenerator.cpp
@@ -3,7 +3,7 @@
 * @brief   Implementation file for class UniqueNameGenerator
 * @date    2010-04-16 21:33:49
 * @author  Tu Yongce <tuyongce@gmail.com>
-* @version $Revision$ $Author$ $Date$
+* @version $Revision$
 */
 
 
